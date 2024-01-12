ae8d2a53c40bd4581e030f4e187e864410d3a419
wang2fromWSL <wang2001zilong@126.com>
2024-01-12 13:12:50 +0800

    1.12 jwt

--- pkg/util/jwt.go
+++ pkg/util/jwt.go
@@ -11,6 +11,7 @@ import (
 // 加载配置文件中设置的密钥
 var jwtSecret = []byte(setting.JwtSecret)
 
+// Claims 定义claims结构体
 type Claims struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -21,6 +22,7 @@ func GenerateToken(username, password string) (string, error) {
 	nowTime := time.Now()
 	expireTime := nowTime.Add(3 * time.Hour)
 
+	//创建 CustomClaims 结构体，用来封装 jwt 信息
 	claims := Claims{
 		username,
 		password,
@@ -30,17 +32,22 @@ func GenerateToken(username, password string) (string, error) {
 		},
 	}
 
+	//创建 header和payload部分
 	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//得到完整的token字符串，这里为加入签名signature部分
 	token, err := tokenClaims.SignedString(jwtSecret)
 
 	return token, err
 }
 
 func ParseToken(token string) (*Claims, error) {
+	//解码过程
 	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		return jwtSecret, nil
 	})
 
+	//验证是否时间过期
 	if tokenClaims != nil {
 		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
 			return claims, nil
